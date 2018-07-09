a6a0fdc1a9c031edc4918d7b163fccb009a6a9ff
unknown <suskudum@WKWIN3282665.global.publicisgroupe.net>
2018-07-09 17:41:43 +0530

    Boostrap 4 assignment

---
+++ js/gulpfile.js
@@ -0,0 +1,28 @@
+gulp.task('default', ['sass', 'browser-sync'], function () {  
+    gulp.watch("scss/*.scss", ['sass']);
+});
+//Seeing it all together
+
+//The entire gulpfile.js should now look like this.
+
+var gulp = require('gulp');  
+var sass = require('gulp-sass');  
+var browserSync = require('browser-sync');
+
+gulp.task('sass', function () {  
+    gulp.src('scss/styles.scss')
+        .pipe(sass({includePaths: ['scss/styles.scss']}))
+        .pipe(gulp.dest('css/styles.css'));
+});
+
+gulp.task('browser-sync', function() {  
+    browserSync.init(["css/*.css", "js/*.js"], {
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('default', ['sass', 'browser-sync'], function () {  
+    gulp.watch("scss/*.scss", ['sass']);
+});
